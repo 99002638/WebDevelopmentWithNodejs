d9d73ebc2c62a4ee364204c1ce367749405e1080
99002638 <71481322+99002638@users.noreply.github.com>
2020-10-18 16:51:55 +0530

    Update Common.js

--- Assignment-2/Script/Common.js
+++ Assignment-2/Script/Common.js
@@ -2,7 +2,7 @@ function $(id){
 	return document.getElementById(id);
 }
 
-//modify the style of display to none, making it invisible
+
 function $hide(id){
 	$(id).style.display = "none"; 
 }
@@ -12,7 +12,7 @@ function $show(id){
 	$(id).style.display = "inline-block";
 }
 
-//The classname to which events have to be bound, for that event, a function should be associated.
+
 function $mapEvent(classname, callbackFn){
 	let allElements = document.getElementsByClassName(classname);
 	for(let i = 0; i < allElements.length; i++){
